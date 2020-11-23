34cb2facee0fa081505feeb50873c117a9d82bbe
Sergey Bershadsky <sbershadsky@anna.money>
2020-11-24 02:11:10 +0300

    use inmem sqlite to test github actions

--- start/tests.py
+++ start/tests.py
@@ -1,10 +1,11 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, override_settings
 from django.urls import reverse
 
 profile_url = reverse('profile')
 login_url = reverse('login')
 
 
+@override_settings(DATABASE={'default': dict(ENGINE='django.db.backends.sqlite3', NAME=':memory:')})
 class ProfileView(SimpleTestCase):
 
     def test_profile_page_unauthorized_redirect(self):
